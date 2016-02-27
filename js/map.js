f6edd64b5f605dc7526b3f00aabbe91e40b8484d
R. Harish Navnit <harishnavnit@gmail.com>
2016-02-27 14:12:04 +0530

    Enable markers and popups on the map

--- js/map.js
+++ js/map.js
@@ -3,7 +3,39 @@
 L.mapbox.projectId = 'rharish.p2i83c48';
 L.mapbox.accessToken = 'pk.eyJ1IjoicmhhcmlzaCIsImEiOiJjaWs4OWl4bGQwMWcydHhrd3ZmczdzN2lsIn0.mCz4ybX25lp0z7-rXXxqiQ';
 
-var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/' + L.mapbox.projectId + '/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
-    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+var mapTiles = L.tileLayer('https://api.mapbox.com/v4/' + L.mapbox.projectId
+                    + '/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
+    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Mapbox</a>'
 });
-var map = L.map('map').addLayer(mapboxTiles).setView([42.3610, -71.0587], 15);
+var map = L.map('map').addLayer(mapTiles).setView([0.0, 0.0], 2);
+
+//FIXME: Do not hardcode values
+var circle = function drawCircle() {
+    L.circle([23.00, -78.00], 500, {
+       color: 'red',
+       fillColor: '#f03',
+       fillOpacity: 0.5
+   }).addTo(map);
+}
+
+//FIXME: Do not hardcode values
+var polygon = function drawPolygon() {
+    return L.polygon([
+        [23.509, -0.08],
+        [23.503, -0.06],
+        [23.51, -0.047]
+    ]).addTo(map);
+}
+
+function onMapClick(e) {
+    //alert("You clicked " + e.latlng);
+    var currLocation_popup = L.popup()
+                                .setLatLng(e.latlng)
+                                .setContent(e.latlng.toString())
+                                .openOn(map);
+    var currLocation_marker= L.marker(e.latlng).addTo(map);
+
+    currLocation_marker.bindPopup(currLocation_popup).openPopup();
+}
+
+map.on('click', onMapClick);
