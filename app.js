c0dd32e9c652df64d28485c788f18373e0adad72
R. Harish Navnit <harishnavnit@gmail.com>
2016-04-19 14:41:45 +0530

    Add CORS headers to the application

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+/**
+ * Middleware to add CORS headers
+ * References:
+ * http://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
+ */
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'OPTIONS',
+                  'PUT', 'PATCH', 'DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With', 'content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/src' +'/index.html'));
     app.use(express.static('src'));
