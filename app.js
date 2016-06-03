ab232a7d9ba4635ed24fd65dced0e2312393d46b
R. Harish Navnit <harishnavnit@gmail.com>
2016-06-03 21:17:56 +0530

    Tiniest of code cleanups

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var path  = require('path');
 var express = require('express');
 var app = express();
-//var server = require('http').Server(app);
 var server = app.listen(3000, function() {
     console.log("App listening on port 3000");
 });
